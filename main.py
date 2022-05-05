4fa1f3321c24461c5afee0408ef4905d20719dc7
debrnr <debrnr@gmail.com>
2022-05-05 21:43:22 +0800

    修改

---
+++ main.py
@@ -0,0 +1,16 @@
+# #!/usr/bin/env python
+# -*- coding: UTF-8 -*- 
+# On 2022/5/4 14:16 Debrnr used the main written by PyCharm on Gamer
+# 从网络下载各种图片文件
+
+class GetImg(object):
+
+    def __init__(self):
+        pass
+
+    def get_baiduimg(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
